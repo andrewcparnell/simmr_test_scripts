bffe3b75ba3238938fcd31eb4f14e545c26157cf
andrewcparnell <andrew.parnell@gmail.com>
2015-09-14 13:41:02 +0100

    Updated support script for Isravia

--- Isaravia_script_20150910.R
+++ Isaravia_script_20150910.R
@@ -11,7 +11,6 @@ consumers <- read.table(text=x,header=TRUE)
 x <- getURL("https://raw.githubusercontent.com/AndrewLJackson/SIAR-examples-and-queries/master/siar-mixing-models/correctionsdemo.txt")
 corr <- read.table(text=x,header=TRUE,stringsAsFactors = F)
 
-
 sm1 <- simmr_load(mixtures=as.matrix(consumers),
                   source_names=sources$Means,
                   source_means = sources[,c(2,4)],
@@ -23,3 +22,33 @@ sm1 <- simmr_load(mixtures=as.matrix(consumers),
 print(sm1)
 
 plot(sm1)
+
+sm1_out = simmr_mcmc(sm1)
+
+require(coda)
+
+traceplot(sm1_out$output[[1]])
+
+is.mcmc.list(sm1_out$output[[1]])
+
+colnames(as.matrix(sm1_out$output[[1]]))
+
+
+x = getURL("https://raw.githubusercontent.com/AndrewLJackson/SIAR-examples-and-queries/master/siar-mixing-models/geese2demo.txt")
+consumers <- read.table(text=x,header=TRUE,stringsAsFactors = FALSE)
+x = getURL("https://raw.githubusercontent.com/AndrewLJackson/SIAR-examples-and-queries/master/siar-mixing-models/concdepdemo.txt")
+concs <- read.table(text=x,header=TRUE,stringsAsFactors = FALSE)
+
+sm2 <- simmr_load(mixtures=as.matrix(consumers[,c(3,2)]),
+                  source_names=sources$Means,
+                  source_means = sources[,c(4,2)],
+                  source_sds = sources[,c(5,3)],
+                  correction_means = corr[,c(4,2)],
+                  correction_sds = corr[,c(5,3)],
+                  concentration_means = concs[,c(4,2)],
+                  group=consumers[,1]
+)
+
+print(sm2)
+
+plot(sm2,group=c(3,5))
